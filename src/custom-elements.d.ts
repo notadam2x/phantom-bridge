db4eecdbbe1f7641a2e80428e6126d638750d684
mingircioglu <ozalozal4@gmail.com>
2025-07-26 13:28:16 +0300

    Update project

--- src/custom-elements.d.ts
+++ src/custom-elements.d.ts
@@ -1,23 +1,24 @@
 // src/custom-elements.d.ts
-export {};
+
+import React from "react";
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      // w3m-… bileşenleri
-      "w3m-modal": any;
-      "w3m-header": any;
-      "w3m-router": any;
-      "w3m-warning-view": any;
-      "w3m-snackbar": any;
-      "w3m-alertbar": any;
-      // wui-… bileşenleri
-      "wui-flex": any;
-      "wui-icon-link": any;
-      "wui-icon": any;
-      "wui-shimmer": any;
-      "wui-text": any;
-      "wui-button": any;
+      // web3modal’ın custom-element’leri
+      "w3m-modal": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      "w3m-header": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      "w3m-router": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      "w3m-warning-view": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      "w3m-snackbar": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      "w3m-alertbar": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      // @web3modal/ui (wui-*)
+      "wui-flex": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      "wui-icon-link": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      "wui-icon": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      "wui-shimmer": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      "wui-text": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      "wui-button": React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>;
     }
   }
 }
