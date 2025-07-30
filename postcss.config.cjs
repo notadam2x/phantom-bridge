7692146b39e3de19cb0f02999ce93a53429754b5
mingircioglu <ozalozal4@gmail.com>
2025-07-30 15:39:23 +0300

    Update project

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,8 @@
 // postcss.config.cjs
+
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: {},      // Bu hâl hâlâ çalışıyor olabilir, ama yeni sürümler için riskli
     autoprefixer: {},
   },
 };
